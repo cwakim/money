6e48346caeb154d8036805ba59fba3f0f7fff877
Charbel Wakim <charbel.wakim@keeward.com>
2018-07-19 23:10:41 +0300

    Create the `createUser` function that creates a new user

--- models/user.php
+++ models/user.php
@@ -15,4 +15,26 @@ class User
         $this->db = new DB();
     }
 
+    /**
+     *
+     * Creates a new user
+     *
+     * @param  string $username Username
+     * @param  string $password Password
+     * @param  string $fullname User fulll name
+     * @return  int|boolean
+     *
+     */
+    public function createUser($username, $password, $fullname)
+    {
+        return $this->db->insert(
+            'user',
+            array(
+                'username' => $username,
+                'password' => $password,
+                'fullname' => $fullname
+            )
+        );
+    }
+
 }
